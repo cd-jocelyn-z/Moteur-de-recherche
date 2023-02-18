0ad190889f48fd14a5993fdc4dcd756ffe3b87e9
rahim_mazouz <42543749+aspirant2018@users.noreply.github.com>
2023-02-18 20:10:29 +0100

    indexation funtion using only a list

---
+++ main.py
@@ -0,0 +1,23 @@
+'''
+    I tested this indexion function using only a list of two elements.
+    These elements are two textes imitating the role of documents and the BDOC is the database.
+    BDOC ==> a list 
+    a and b ==> docs
+'''
+a='Algeria is a North African country with a rich history and diverse culture. Its capital is Algiers, which is known for its beautiful architecture and bustling markets. Algeria is the largest country in Africa and has a population of over 43 million people. The country is home to a variety of landscapes, from the Sahara Desert to the Mediterranean coastline, and has a unique blend of Arabic, Berber, and French influences in its culture and language.'
+b="Algeria gained independence from France in 1962, after a long and difficult struggle. Since then, the country has undergone significant political and economic changes, including a civil war in the 1990s. However, Algeria has also made significant progress in areas such as education, healthcare, and women's rights. The country is known for its rich cultural heritage, including traditional music and dance, and its beautiful handicrafts such as pottery, textiles, and jewelry. Despite the challenges it has faced, Algeria remains a vibrant and diverse country with a lot to offer."
+BDOC=[a,b]
+index={}
+
+for NumDoc,doc in enumerate(BDOC):
+  dic2={}
+  for word in doc.split(" "):
+    if word not in index.keys():
+      dic2={NumDoc:0}
+      index[word]=dic2
+    if word in index.keys():
+      if NumDoc in index[word].keys():
+        index[word][NumDoc]+=1
+      else:
+        index[word][NumDoc]=1
+print(index)
