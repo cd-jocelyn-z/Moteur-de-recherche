040fac2e2c1719c09fde7be199726686fee4d8e1
rahim_mazouz <42543749+aspirant2018@users.noreply.github.com>
2023-03-04 14:32:17 +0100

    function that takes a file as un arguments and returns a dictionnary {NumArticle : Content}

--- readfile.py
+++ readfile.py
@@ -1,7 +1,38 @@
+import bs4
+
+# Load the TEI.2 file
 def readfile(filename):
     with open(filename, 'r') as file:
-        data = file.read()
-    return data
+        xml = file.read()
+  
+    return xml
+BDOC = {}
+xml = readfile(".\\AMARYLLIS-98-extrait-OFIL 2\\OFIL\\OD1")
+soup=bs4.BeautifulSoup(xml,"lxml")
+articles=soup.find_all('div',{'type':'article'})
+print(len(articles))
+for article in articles:
+    NumArticle=article.get('id')
+    try:
+        Title=article.title.get_text()
+    except AttributeError:
+        print("No title element found in article:", article.get('id'))
+        Title=" "
 
-print(readfile("doc1.txt"))
+    try:
+        Paragraphe=article.p.get_text()
+    except AttributeError:
+        print("No title Paragraphe element found in article:", article.get('id'))
+        Paragraphe=" "
+    
+    BDOC[NumArticle]=Title+' '+Paragraphe
+
+print(len(BDOC))
+   
+
+
+
+
+
+
 
