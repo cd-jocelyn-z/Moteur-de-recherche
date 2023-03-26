cdbaf356ff9246b01b2f337d73088e3bb1d8b6c7
rahim_mazouz <42543749+aspirant2018@users.noreply.github.com>
2023-03-26 12:26:14 +0200

    deleting the indexation function

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import readfile
-import re
+
+import indexation
+import tf
+
 
 print("reading the content of the files.....")
 BDOC = readfile.readfile(".\\AMARYLLIS-98-extrait-OFIL 2\\OFIL\\OD1")
@@ -7,30 +10,16 @@ print("end of reading")
 
 ## PHASE D'INDEXATION
 
-index={}
+index=indexation.indexation(BDOC)
 
-for NumArticle in BDOC.keys():
-  dic2={}
-  for word in re.split(r'[,.?\s\']+',BDOC[NumArticle]):
-    if word not in index.keys():
-      dic2={NumArticle:0}
-      index[word]=dic2
-    if word in index.keys():
-      if NumArticle in index[word].keys():
-        index[word][NumArticle]+=1
-      else:
-        index[word][NumArticle]=1
+print(tf.tf(index,"mail"))
 
-        
-print("le mot 'il'")
-print(index["il"])
 
 ## PHASE D'INTERROGATION
 
-requete="Algeria economic"
+requete="france italie"
 result={}
 for WordRequete in requete.split(' '):
-  print(WordRequete)
   for Word in index.keys():
     if WordRequete == Word:
       result[Word]=index.get(WordRequete)
